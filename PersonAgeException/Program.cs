fde296e8012ef251af2d1e2803eba1c1ded5830a
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-26 15:50:31 +0530

    [Ritvik]Add: Welcome Message

---
+++ PersonAgeException/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonAgeException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Age Determination");
+        }
+    }
+}
